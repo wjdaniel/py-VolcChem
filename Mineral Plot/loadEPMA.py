c493b950bd846d5702ffae98104716942bfce0c8
wjdaniel <wjleedaniel@gmail.com>
2023-01-01 23:36:36 +0800

    trial

--- Mineral Plot/loadEPMA.py
+++ Mineral Plot/loadEPMA.py
@@ -1,7 +1,12 @@
 
 import os
-def csvLoad(fileName):
+import pandas as pd
+
+def excelLoad(sheetNum):
     tempDir=os.getcwd()
-    dataFile='%s\\data\\' % tempDir
+    dataFile='%s\\data\\masterFile.xlsx' % tempDir
+    tempFile=pd.read_excel(dataFile, sheet_name=sheetNum-1,header=0)
+    outFile=tempFile.fillna(0)
+    return outFile
 
     
